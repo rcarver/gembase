b7df153c9b780403ac4ae32a2aaefe2e2e7e7fff
Ryan Carver <ryan@typekit.com>
2011-05-05 17:24:01 -0700

    make a Rakefile too

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
-echo 'Updating Rakefile.base'
-curl -sO "https://github.com/rcarver/gembase/raw/master/Rakefile.base"
-
 if [ ! -d .git ]; then
   dir=$(cd `dirname $0` && pwd)
   name=`basename $dir`
   echo "Initializing $name with bundle gem"
   cd .. && bundle gem $name
+  cd $name
 fi
+
+echo "Initializing Rakefile"
+echo 'load "Rakefile.base"' > Rakefile
+
+echo 'Updating Rakefile.base'
+curl -sO "https://github.com/rcarver/gembase/raw/master/Rakefile.base"
