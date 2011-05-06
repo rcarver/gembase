70c8505410262664db64a8976d189fba48f36598
Ryan Carver <ryan@typekit.com>
2011-05-05 17:34:16 -0700

    create changelog, readme and commit it

--- install.sh
+++ install.sh
@@ -12,4 +12,11 @@ echo 'load "Rakefile.base"' > Rakefile
 echo 'Creating Rakefile.base'
 curl -sO "https://github.com/rcarver/gembase/raw/master/Rakefile.base"
 
+echo 'Creating CHANGELOG.md'
+echo "0.0.1 / `date '+%Y-%m-%d'`" > CHANGELOG.md
+
+echo 'Creating README.md'
+echo "# $name" > README.md
+
 git add .
+git commit -m "Initialize with gembase"
